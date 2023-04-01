548d8299b9c80bee4590776b59fb07328af44ce4
zfletch <zfletch2@gmail.com>
2023-04-01 09:40:45 -0400

    initial commit

---
+++ spec/llama/rb_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Llama::Rb do
+  it "has a version number" do
+    expect(Llama::Rb::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
