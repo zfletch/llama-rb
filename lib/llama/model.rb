501da5ae4022cb76333d0f725cb2885ccb7044c8
zfletch <zfletch2@gmail.com>
2023-04-02 14:44:03 -0400

    suppress stderr from llama.cpp

--- lib/llama/model.rb
+++ lib/llama/model.rb
@@ -1,3 +1,5 @@
+require "tempfile"
+
 module Llama
   class Model
     # move methods defined in `model.cpp` from public to private
@@ -6,13 +8,40 @@ module Llama
     def self.new
       instance = allocate
       instance.send(:initialize)
-      instance.send(:initialize_cpp)
+
+      instance.send(:capture_stderr) do
+        instance.send(:initialize_cpp)
+      end
 
       instance
     end
 
     def predict(prompt)
-      predict_cpp(prompt)
+      capture_stderr { predict_cpp(prompt) }
+    end
+
+    private
+
+    attr_reader :stderr
+
+    def capture_stderr
+      previous = STDERR.dup
+      tmp = Tempfile.open("llama-rb-stderr")
+      result = nil
+
+      begin
+        STDERR.reopen(tmp)
+
+        result = yield
+
+        tmp.rewind
+        @stderr = tmp.read
+      ensure
+        tmp.close(true)
+        STDERR.reopen(previous)
+      end
+
+      result
     end
   end
 end
