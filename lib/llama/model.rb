e00868ea3583999d6fe3477c04b95ca4c636cf3e
zfletch <zfletch2@gmail.com>
2023-04-07 18:14:08 -0400

    support additional flags

--- lib/llama/model.rb
+++ lib/llama/model.rb
@@ -6,29 +6,61 @@ module Llama
     class ModelError < StandardError
     end
 
-    def initialize(
+    def initialize( # rubocop:disable all
       model,
+      binary: default_binary,
       seed: Time.now.to_i,
-      n_predict: 128,
-      binary: default_binary
+      n_predict: nil,
+      threads: nil,
+      top_k: nil,
+      top_p: nil,
+      repeat_last_n: nil,
+      repeat_penalty: nil,
+      ctx_size: nil,
+      ignore_eos: nil,
+      memory_f32: nil,
+      temp: nil,
+      n_parts: nil,
+      batch_size: nil,
+      keep: nil,
+      mlock: nil
     )
       @model = model
       @seed = seed
       @n_predict = n_predict
       @binary = binary
+      @threads = threads
+      @top_k = top_k
+      @top_p = top_p
+      @repeat_last_n = repeat_last_n
+      @repeat_penalty = repeat_penalty
+      @ctx_size = ctx_size
+      @ignore_eos = ignore_eos
+      @memory_f32 = memory_f32
+      @temp = temp
+      @n_parts = n_parts
+      @batch_size = batch_size
+      @keep = keep
+      @mlock = mlock
     end
 
     def predict(prompt)
       stdout, @stderr, @status = Open3.capture3(command(prompt))
 
-      raise ModelError, "Error #{status.to_i}" unless status.success?
+      unless status.success?
+        error_string = stderr.split("\n").first
+
+        raise ModelError, "Error #{error_string}"
+      end
 
       # remove the space that is added as a tokenizer hack in examples/main/main.cpp
       stdout[0] = ''
       stdout
     end
 
-    attr_reader :model, :seed, :n_predict, :binary
+    attr_reader :model, :seed, :n_predict, :binary, :threads, :top_k, :top_p, :repeat_last_n,
+      :repeat_penalty, :ctx_size, :ignore_eos, :memory_f32, :temp, :n_parts, :batch_size, :keep,
+      :mlock
 
     private
 
@@ -38,19 +70,42 @@ module Llama
       File.join(File.dirname(__FILE__), '..', '..', 'bin', 'llama')
     end
 
-    def command(prompt)
-      escape_command(binary,
+    def command(prompt) # rubocop:disable all
+      escape_command(
+        binary,
         model: model,
         prompt: prompt,
         seed: seed,
-        n_predict: n_predict)
+        n_predict: n_predict,
+        threads: threads,
+        top_k: top_k,
+        top_p: top_p,
+        repeat_last_n: repeat_last_n,
+        repeat_penalty: repeat_penalty,
+        ctx_size: ctx_size,
+        'ignore-eos': !!ignore_eos,
+        memory_f32: !!memory_f32,
+        temp: temp,
+        n_parts: n_parts,
+        batch_size: batch_size,
+        keep: keep,
+        mlock: mlock,
+      )
     end
 
     def escape_command(command, **flags)
-      flags_string = flags.map do |key, value|
-        "--#{Shellwords.escape(key)} #{Shellwords.escape(value)}"
-      end.join(' ')
+      flags_components = []
+
+      flags.each do |key, value|
+        if value == true
+          flags_components.push("--#{Shellwords.escape(key)}")
+        elsif value
+          flags_components.push("--#{Shellwords.escape(key)} #{Shellwords.escape(value)}")
+        end
+      end
+
       command_string = Shellwords.escape(command)
+      flags_string = flags_components.join(' ')
 
       "#{command_string} #{flags_string}"
     end
--- spec/llama/model_spec.rb
+++ spec/llama/model_spec.rb
@@ -1,11 +1,42 @@
 RSpec.describe Llama::Model do
-  subject(:model) { described_class.new('models/7B/ggml-model-q4_0.bin', seed: 10, n_predict: 1) }
+  subject(:model) { described_class.new('models/7B/ggml-model-q4_0.bin', seed: 5, **params) }
+
+  let(:params) { {} }
 
   it 'predicts text' do
     expect(
-      model.predict('hello, wo'),
-    ).to eq(
-      'hello, woof',
+      model.predict('test'),
+    ).to include(
+      "test 3 7/12/18\nIn Reply to: test 3 posted by test on July 04, 2018 at 19:05:16:",
     )
+  end
+
+  context 'with all supported flags' do
+    let(:params) do
+      {
+        n_predict: 3,
+        threads: 8,
+        top_k: 41,
+        top_p: 0.8,
+        repeat_last_n: 63,
+        repeat_penalty: 1.2,
+        ctx_size: 511,
+        ignore_eos: true,
+        memory_f32: true,
+        temp: 0.9,
+        n_parts: 10,
+        batch_size: 7,
+        keep: -1,
+        mlock: true,
+      }
+    end
+
+    it 'predicts text' do
+      expect(
+        model.predict('hello, wo'),
+      ).to eq(
+        'hello, woofs!',
+      )
+    end
   end
 end
